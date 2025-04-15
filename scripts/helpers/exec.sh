6ae1ca61297f4879ca1e809d34893d71e9dcfd9d
shandilya1998 <shandilya.shreyas@gmail.com>
2025-04-15 21:40:53 +0530

    SITL integration and scripts reorganisation
    Problem
    =======
    1. Needed to include `iexec` in scripts to be run from outside `djinn`
    2. Docker container startup for ardupilot and airsim needs to be
       implemtend
    3. Bug observed in platform flag propagation in `djinn build docker`
    4. Workflow for starting sitl needs to be implemented
    
    Solution
    ========
    1. Created `scripts/helpers/` to organise all helper scripts meant to
       be sourced within djinn and all its derivative scripts within the
       workspace
    2. Moved instructions related to variable creation and data sourcing
       from `djinn` to `scripts/helpers/data.sh`
    3. Moved instructions related to execution within docker containers from
       `djinn` to `scripts/helpers/exec.sh`
    4. Added options `djinn up ardupilot` and `djinn up airsim` in `djinn`
       to start containers for ardupilot and airsim
    5. Added options to build ardupilot and airsim docker images in `djinn`.
       The following commands were introduced: `djinn init docker airsim`
       and `djinn init docker ardupilot`
    6. Added platform handling in `djinn` for the command `djinn build
       docker`
    7. Implemented the command `djinn start sitl` in `djinn`
    
    Note
    ====
    1. Error in path of `scripts/docker-setup.sh` in `djinn up nrt` removed

---
+++ scripts/helpers/exec.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is helper script for djinn, meant only to be sourced within djinn and other scripts that source $NRT_WS/scripts/data.sh
+
+iexec(){
+            time docker container exec -it "$1" /bin/bash -l -c "$2"
+        }
+
+iexec_ros(){
+			export $(grep -v '^#' $config_file | xargs)
+			time docker container exec -it "$1" /bin/bash -l -c "source /opt/ros/${ROS_VERSION}/setup.bash && $2"
+        }
+
+iexec_kalibr(){
+			time docker container exec -it "$1" /bin/bash -l -c "source /opt/ros/noetic/setup.bash && source \$WORKSPACE/devel/setup.bash && $2"
+        }
