05e33779579281a6ca8dbc1cc28066c37006a9e5
wasp-yash <trooper0018080@gmail.com>
2026-02-08 20:48:35 +0530

    removed comments
    Signed-off-by: wasp-yash <trooper0018080@gmail.com>

--- scripts/build_ros_packages.sh
+++ scripts/build_ros_packages.sh
@@ -1,24 +1,3 @@
-# #!/bin/bash
-
-# export CMAKE_PREFIX_PATH=/Pangolin/build:$CMAKE_PREFIX_PATH
-# export CMAKE_PREFIX_PATH=/opencv:$CMAKE_PREFIX_PATH
-
-
-
-# echo "===============Building ROS2 Package=============="
-# cd /ws/ros_ws
-# echo "***********************************************"
-# echo $PWD
-# cd /ws/ros_ws/src/slam/ext/MORB_SLAM/
-# ./build.sh -DCMAKE_BUILD_TYPE=RELEASE -j7
-# cd /ws/ros_ws
-# echo "***********************************************"
-# # rm -rf build install
-# colcon build --symlink-install --cmake-args '-DCMAKE_EXPORT_COMPILE_COMMANDS=1' '-DCMAKE_BUILD_TYPE=Release' '-Wno-dev' --mixin debug
-
-#!/bin/bash
-
-# Set environment variables
 export CMAKE_PREFIX_PATH=/Pangolin/build:$CMAKE_PREFIX_PATH
 export CMAKE_PREFIX_PATH=/opencv:$CMAKE_PREFIX_PATH
 
